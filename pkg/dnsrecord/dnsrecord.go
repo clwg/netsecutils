7598b3a87bf967bfc6b006cc4731425efaec0601
Chris <chris@layer8.org>
2023-11-29 06:44:24 +0000

    Remove unused code for querying DNS records

--- pkg/dnsrecord/dnsrecord.go
+++ pkg/dnsrecord/dnsrecord.go
@@ -1,11 +1,5 @@
 package dnsrecord
 
-import (
-	"fmt"
-
-	"github.com/miekg/dns"
-)
-
 type DNSRecords struct {
 	SOA   []SOARecord   `json:"soa"`
 	MX    []MXRecord    `json:"mx"`
@@ -15,19 +9,4 @@ type DNSRecords struct {
 	TXT   []TXTRecord   `json:"txt"`
 	SRV   []SRVRecord   `json:"srv"`
 	CNAME []CNAMERecord `json:"cname"`
-}
-
-func getDNSRecords(domain string, qtype uint16) []dns.RR {
-	var records []dns.RR
-	m := new(dns.Msg)
-	m.SetQuestion(dns.Fqdn(domain), qtype)
-
-	in, err := dns.Exchange(m, "1.1.1.1:53")
-	if err != nil {
-		fmt.Printf("Error querying DNS: %s\n", err)
-		return records
-	}
-
-	records = append(records, in.Answer...)
-	return records
 }
